83867f89ed0da931080f534ee3f2fc35cf3f216a
Royce119 <madcoder911@gmail.com>
2023-08-05 23:25:02 +0300

    finished db setup and added notification component

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,17 @@
 import { MongoClient } from "mongodb";
+
+const connectDatabase = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://test2:test2@cluster1.656gljt.mongodb.net/events?retryWrites=true&w=majority"
+  );
+  return client;
+};
+const insertDocument = async (client, doc) => {
+  const db = client.db();
+
+  await db.collection("emails").insertOne(doc);
+  client.close();
+};
 const handler = async (req, res) => {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -8,16 +21,21 @@ const handler = async (req, res) => {
     }
 
     //Mongo DB
-
-    const client = await MongoClient.connect(
-      "mongodb+srv://test2:test2@cluster1.656gljt.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
-
-    await db
-      .collection("emails")
-      .insertOne({ email: userEmail, time: new Date().toISOString() });
-    client.close();
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Couldn't establish a connection with the database" });
+      return;
+    }
+    try {
+      await insertDocument(client, { email: userEmail });
+    } catch (err) {
+      res.status(500).json({ message: "Inserting data failed." });
+      return;
+    }
 
     res
       .status(201)
